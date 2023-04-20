eee48c0f9dc93745ca3a5b7ca3d98fd8a94de55f
shepherrrd <shefkid2@gmail.com>
2023-04-20 15:39:51 +0100

    Finished

--- Services/deleteService.js
+++ Services/deleteService.js
@@ -1,8 +1,8 @@
 const pool = require("../database/index")
-async function deletef(res,req) {
+async function deletef(req,res) {
     try {
         const {id} = req.params
-        const [row,fields] = await pool.query("delete  from blog where id = ?", [id])
+        const [row,fields] = await pool.query("delete from blog where id = ?", [id])
         res.json(
             {data : row})
     } catch (error) {
